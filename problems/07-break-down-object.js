40e84bc0acc12e0fb72fd2e9e4a76bed0862c036
Cindy Rose Guzman <cindyrose.go@gmail.com>
2022-06-14 14:50:20 -0700

    solved problem 7

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function `breakDownObj(obj)` that takes in an object as a parameter 
-and returns an array containing:  all the keys from the object **and** all the 
+Write a function `breakDownObj(obj)` that takes in an object as a parameter
+and returns an array containing:  all the keys from the object **and** all the
 values of the object.
 
 **Hint**: Use spread syntax to spread out elements into an array!
@@ -13,9 +13,17 @@ breakDownObj(object1); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
 let object2 = {location: 'NY', borough: 'Brooklyn'};
 breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 ***********************************************************************/
-
+//POLYA
+// iterate thru obj, and push each key and value separately, using spread operator
 function breakDownObj(obj) {
-  // Your code here
+  let keys = []
+  let values = []
+  for (let key in obj){
+    if (key) keys.push(key)
+    if (obj[key]) values.push(obj[key])
+  }
+  let keyValues = [...keys, ...values]
+  return keyValues
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
